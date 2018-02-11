0165410e118c1ace793586f7d1ca3f0459478468
Benjamin Amor <bamor713@gmail.com>
2018-02-11 22:04:38 +0000

    feature: added animated login screen

--- App/app/src/main/java/flow/app/home/HomeActivity.java
+++ App/app/src/main/java/flow/app/home/HomeActivity.java
@@ -1,13 +1,15 @@
-package flow.app;
+package flow.app.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import flow.app.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
