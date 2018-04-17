6e2c9437fb0520e4f1e2d21ef042d5aeec8b80e2
Benjamin Amor <bamor713@gmail.com>
2018-04-17 13:11:09 +0100

    feature: profile page layout

--- App/app/src/main/java/flow/app/profile/MyProfileActivity.java
+++ App/app/src/main/java/flow/app/profile/MyProfileActivity.java
@@ -14,5 +14,7 @@ public class MyProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+        
     }
 }
